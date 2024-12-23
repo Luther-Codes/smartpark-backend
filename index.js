73c887df1cab030d9cab1301d9e9811107b17e85
VincentiusJacob <142817267+VincentiusJacob@users.noreply.github.com>
2024-12-23 11:18:29 +0700

    latest commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://binus-smartpark.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
